16ae240a059edc89f0aeaf70f436ee8d924ce9d1
vannykin <e0969161@u.nus.edu>
2024-06-21 20:03:09 +0800

    change wanted tags to extract qoo10

--- extracty/scraper.py
+++ extracty/scraper.py
@@ -70,7 +70,7 @@ class WebScraper:
         return cleaned_content
 
     def scraping_with_langchain(
-        self, wanted_tags: list[str] = ["title", "meta", "span", "div", "li", "p", "h2"]
+        self, wanted_tags: list[str] = ["span", "div", "a", "p", "h2", "h3", "table", "strong"]
     ):
         """
         Scrapes the content of a web page using Requests.
