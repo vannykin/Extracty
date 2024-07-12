178be81f51e0b74581c3b9c30d96c52067d1b69b
vannykin <e0969161@u.nus.edu>
2024-07-12 11:04:36 +0800

    Troubleshooting...

--- extracty/llm_extractor.py
+++ extracty/llm_extractor.py
@@ -193,4 +193,4 @@ class LLMExtractor:
         # )
 
         # TODO: implement more logic to handle response and create a structured output
-        return content[:10]
+        return content[:100]
